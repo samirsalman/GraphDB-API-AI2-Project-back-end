b6eae245ccade490b715084301d21d1238cd1488
samirsalman <33979978+samirsalman@users.noreply.github.com>
2020-01-27 17:23:40 +0100

    Update queryStrings.js

--- api/models/queryStrings.js
+++ api/models/queryStrings.js
@@ -283,48 +283,52 @@ class QueryStrings {
     var titleParts = title.split(" ");
     var filtering = "";
     for (var j = 0; j < titleParts.length; j++) {
-      if (j != titleParts.length - 1) {
-        filtering += `(regex(?title, "${titleParts[j]}", "i")) || `;
-      } else {
-        filtering += `(regex(?title, "${titleParts[j]}", "i"))`;
+      if (j !== titleParts.length - 1 && titleParts[j].length > 3) {
+        filtering += `(regex(?title, "${this.replaceTitleString(
+          titleParts[j]
+        )}", "i")) ||`;
+      } else if (j === titleParts.length - 1 && titleParts[j].length > 3) {
+        filtering += `(regex(?title, "${this.replaceTitleString(
+          titleParts[j]
+        )}", "i"))`;
       }
     }
 
     return `PREFIX bibo: <http://purl.org/ontology/bibo/>
-    PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    PREFIX dc0: <http://purl.org/dc/terms/>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX dc: <http://purl.org/dc/elements/1.1/>
+  PREFIX dc0: <http://purl.org/dc/terms/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
 
-    select ?book ?type ?title ?name ?year ?isbn ?pub ?issn ?edit ?journ ?booktitle where { 
-        ?book a bibo:Document .
-        ?book a ?type .
-        ?book dc:title ?title .
-        
-        OPTIONAL{
-          ?book dc:date ?year .
-          ?book dc0:creator ?authors .
-          ?authors foaf:name ?name .
-          OPTIONAL {  ?book dc0:publisher ?pub .}
-          OPTIONAL { ?book bibo:booktitle ?booktitle .}
-          OPTIONAL { ?book bibo:journaltitle ?journ .}
-          OPTIONAL {?book dc0:editor ?edit .}
-          OPTIONAL {?book bibo:isbn ?isbn .}
-          OPTIONAL {?book bibo:issn ?issn .}
-          }
-
-          FILTER NOT EXISTS{
-            ?subtype ^a ?book.
-            ?subtype rdfs:subClassOf ?type .
-            filter(?subtype != ?type) .
+  select ?book ?type ?title ?name ?year ?isbn ?pub ?issn ?edit ?journ ?booktitle where { 
+      ?book a bibo:Document .
+      ?book a ?type .
+      ?book dc:title ?title .
+      
+      OPTIONAL{
+        ?book dc:date ?year .
+        ?book dc0:creator ?authors .
+        ?authors foaf:name ?name .
+        OPTIONAL {  ?book dc0:publisher ?pub .}
+        OPTIONAL { ?book bibo:booktitle ?booktitle .}
+        OPTIONAL { ?book bibo:journaltitle ?journ .}
+        OPTIONAL {?book dc0:editor ?edit .}
+        OPTIONAL {?book bibo:isbn ?isbn .}
+        OPTIONAL {?book bibo:issn ?issn .}
         }
-        
-        FILTER (${filtering}) .
-        FILTER (?book != <${uri}>) .
 
-    } `;
+        FILTER NOT EXISTS{
+          ?subtype ^a ?book.
+          ?subtype rdfs:subClassOf ?type .
+          filter(?subtype != ?type) .
+      }
+      
+      FILTER (${filtering}) .
+      FILTER (?book != <http://purl.org/ontology/bibo/${uri}>) .
+
+  } `;
   };
 }
 
